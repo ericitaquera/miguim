c40e2493da0d51f85f88ff878dd6cb5347f3d7c1
ericitaquera <ericitaquera@gmail.com>
2020-08-25 11:28:42 +0000

    third commit

--- folders.tf
+++ folders.tf
@@ -2,7 +2,7 @@ module "folders" {
   source  = "terraform-google-modules/folders/google"
   version = "~> 2.0"
 
-  parent  = "folders/65552901371"
+  parent  = "folders/"
 
   names = [
     "miguim-folder"
